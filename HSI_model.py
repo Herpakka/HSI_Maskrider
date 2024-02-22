cb7d61afc6532f0e133d680774026e6fc7472d85
Herpakka <106274468+Herpakka@users.noreply.github.com>
2024-02-23 04:32:19 +0700

    Update HSI_model.py

--- HSI_model.py
+++ HSI_model.py
@@ -5,12 +5,12 @@ import os
 
 import beta1_support as control
 
+image = None
 result = None
 h,w = 0,0
 class process:
-    
     def rgb_to_hsi(img):
-        global result
+        global result,image
         H1 = int((control.h)/2)
         H2 = int((control.h2)/2)
         sat1 = int(float(control.s)*255)
@@ -61,6 +61,40 @@ class process:
         w = int(result.shape[1])
         print ("h = ",h,"w = ",w)
         return h, w
+    def display_mask(img):
+        H1 = int((control.h)/2)
+        H2 = int((control.h2)/2)
+        sat1 = int(float(control.s)*255)
+        sat2 = int(float(control.s2)*255)
+        inten1 = int(control.v)
+        inten2 = int(control.v2)
+        print("Image path =",img)
+        # img = Image.open(img)
+        image = cv2.imread(img)
+        # แปลงภาพจาก RGB เป็น HSI(HSV)
+        hsi = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        #HSI(HSV) ช่วงของ Hue 0-180
+        #Saturation เป็น 0-255 = 0-100%
+        #Intensity เป็น 0-255 = 0-100%
+        lower = np.array([H1, sat1, inten1])
+        upper = np.array([H2, sat2, inten2])
+        print("lower :",lower,"upper :",upper)
+
+        # สร้าง mask สำหรับ hsi
+        mask = cv2.inRange(hsi, lower, upper)
+
+        # ตัดภาพเฉพาะส่วน ที่ในช่วงที่กำหนดของ HSI
+        cut = cv2.bitwise_or(image, image, mask=mask)
+
+        # สร้างภาพใหม่และแทนที่พิกเซลด้วยสีเขียว
+        result = image.copy()
+        result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+        pixels = np.where(mask != 0)
+        result[pixels] = (0, 255, 0)
+
+        print("size = ",result.shape)
+        return result
 
     # # อ่านภาพ
     # image_path = '2023-01-31_13.42.40.png'
